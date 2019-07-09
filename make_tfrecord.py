00d52c56464e42f6ef8e71f06ceb5e5190190e0a
panxin801 <1457353243@qq.com>
2019-07-09 17:25:49 +0800

    know how to do with relations between clean and noise

--- make_tfrecord.py
+++ make_tfrecord.py
@@ -5,57 +5,18 @@ import numpy as np
 import shutil
 import tensorflow as tf
 
-parser = argparse.ArgumentParser(description='Spec Augment')
-parser.add_argument(
-    '--audio-path', default='/home/panxin', help='The audio file.')
 
-args = parser.parse_args()
-
-
-def writeTXT(writer, tagid, warped_masked_spectrogram):
-    print("%s  [" % (tagid), file=writer, flush=True)
-    for y in range(0, warped_masked_spectrogram.shape[1]):
-        writer.write("  ")
-        for x in range(0, warped_masked_spectrogram.shape[0]):
-            writer.write("%f " % (warped_masked_spectrogram[x][y]))
-        if (y == warped_masked_spectrogram.shape[1] - 1):
-            print("]", file=writer, flush=True)
-        else:
-            writer.write("\n")
-
-
-# The following new adding func
-def checkSaveFile(writeFile):
-    if (os.path.exists(writeFile)):
-        writeNew = writeFile + "new"
-        writer = open(writeNew, "wt")
-        for line in open(writeFile):
-            if "[" in line:
-                retId = line.split("  ")[0]
-        for line in open(writeFile):
-            if "{}  [".format(retId) in line:
-                break
-            else:
-                print(line, end="", file=writer)
-        writer.close()
-        shutil.move(writeNew, writeFile)
-        return retId
-
-
-def wrif(writerList, readList, i):
-    print("{} read {}, write {}".format(i, readList[i], writerList[i]))
-    # Next line can be comment, when dealing with normal ending
-    retId = checkSaveFile(writerList[i])
+def ReadARKFile(readFile):
     valueList = []
     num = 0
-    writer = open(writerList[i], "at")
-    reachRetId = 0
-    for line in open(readList[i]):
-        if reachRetId == 0:
+    # ID of what started, and care about the bits
+    searchRetId = 0
+    for line in open(readFile, "rt"):
+        if searchRetId == 0:
             if "{}  [".format(retId) not in line:
                 continue
             else:
-                reachRetId = 1
+                searchRetId = 1
         if "[" in line:
             tagid = line.split(" ")[0]
             if (tagid < retId):
@@ -70,20 +31,19 @@ def wrif(writerList, readList, i):
                 mel_spectrogram=mel_spectrogram,
                 time_warping_para=5,
                 time_masking_para=15)
-            writeTXT(writer, tagid, warped_masked_spectrogram)
+
             valueList.clear()
             num += 1
-            if (num % 100 == 0):
+            if (num % 500 == 0):
                 print(num)
         else:
             Tmpline = line.strip()
             valueList.append(Tmpline.split(" "))
-    writer.close()
 
 
 def main():
-    currentdir = os.path.dirname(os.path.abspath(__file__))
-    data_dir = os.path.join(currentdir, "data")
+    currentDir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(currentDir, "data")
     clean_dir = os.path.join(data_dir, "clean")
     noise_dir = os.path.join(data_dir, "noise")
 
@@ -96,8 +56,16 @@ def main():
         if file.endswith(".txt")
     ]
 
-    outTFrecord = "output_TFRecord.tfrecords"
-    outputPath = os.path.join(data_dir, outTFrecord)
+    # Make noise_feats.scp dict
+    noise_feats = os.path.join(data_dir, "noise_feats.scp")
+    noise_feats_dict = {}
+    with open(noise_feats, "rt", encoding="utf-8") as f:
+        for line in f.readlines():
+            key, val = line.split()
+            noise_feats_dict.update({key: val})
+
+    outTFRecord = "output_TFRecord.tfrecords"
+    outputPath = os.path.join(data_dir, outTFRecord)
     if os.path.exists(outputPath):
         os.unlink(outputPath)  # Delete $outputPath this file
         # raise ValueError(
@@ -105,11 +73,23 @@ def main():
         #         outputPath))
     # TODO set some cover or judge or delete mechanism with $outputPath( this file )
     output_file = tf.python_io.TFRecordWriter(outputPath)
-    for m, wave_file in enumerate(clean_list):
-        print("{} ---- {}".format(m, wave_file))
+    for __, clean_ARK_file in enumerate(clean_list):
+        ReadARKFile(clean_ARK_file)
 
+    output_file.close()
     print("Done!!")
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Convert txt featyre to TFRecord.')
+    parser.add_argument(
+        '--audio-path', default='/home/panxin', help='The audio file.')
+    parser.add_argument(
+        '--force-gen',
+        dest='force_gen',
+        action='store_true',
+        help='Flag to force overwriting existing dataset.')
+    parser.set_defaults(force_gen=True)
+    args = parser.parse_args()
     main()
