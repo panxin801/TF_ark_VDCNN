44333de2acd7905e29328b294b9d0c52d98ca88a
panxin801 <1457353243@qq.com>
2019-07-14 23:14:06 +0800

    train_main.py has a bug, but values can be printed

--- train_main.py
+++ train_main.py
@@ -12,35 +12,42 @@ parser.add_argument(
 args = parser.parse_args()
 
 
+def _parse_record(example_proto):
+    features = {
+        "wav_feat": tf.FixedLenFeature([], tf.string),
+        "noisy_feat": tf.FixedLenFeature([], tf.string)
+    }
+    parsed_features = tf.parse_single_example(example_proto, features=features)
+    return parsed_features
+
+
 def read_and_decode(TFRecordqueue, context_window_size, feat_size):
-    reader = tf.TFRecordReader()
-    _, serialized_example = reader.read(TFRecordqueue)
-    features = tf.parse_single_example(
-        serialized_example,
-        features={
-            "wav_feat": tf.FixedLenFeature([], tf.string),
-            "noisy_feat": tf.FixedLenFeature([], tf.string)
-        })
-    clean_feats = tf.decode_raw(features["wav_feat"],
-                                tf.half)  # tf.half===tf.float16
-    noise_feats = tf.decode_raw(features["noisy_feat"], tf.half)
-    # Check here http://osask.cn/front/ask/view/289151
-    clean_feats.set_shape([context_window_size * feat_size])
-    sliced_feat = tf.reshape(clean_feats, [context_window_size, feat_size])
-    noise_feats.set_shape([context_window_size * feat_size])
-    sliced_noise_feat = tf.reshape(noise_feats,
-                                   [context_window_size, feat_size])
-    return sliced_feat, sliced_noise_feat
+    dataset = tf.data.TFRecordDataset(TFRecordqueue)
+    dataset = dataset.map(_parse_record)
+    iterator = dataset.make_one_shot_iterator()
+    with tf.Session() as sess:
+        features = sess.run(iterator.get_next())
+
+        # clean_feats = features['wav_feat']
+        # clean_feats = np.fromstring(clean_feats)
+        # sliced_feat = np.reshape(clean_feats, [context_window_size, feat_size])
+        # print('=======')
+        clean_feats = tf.decode_raw(features["wav_feat"],
+                                    tf.half)  # tf.half===tf.float16
+        # Check here http://osask.cn/front/ask/view/289151
+        clean_feats.set_shape([context_window_size * feat_size])
+        sliced_feat = tf.reshape(clean_feats, [context_window_size, feat_size])
+        print(sess.run(sliced_feat))
+
+    return sliced_feat
 
 
 def main(_):
     currentPath = os.path.dirname(os.path.abspath(__file__))
     TFRecord = os.path.join(currentPath, os.path.join("data", args.TFRecord))
     TFRecordqueue = tf.train.string_input_producer([TFRecord])
-    clean_feats, noise_feats = read_and_decode(
-        TFRecordqueue, 11, 43)  # 11 means context window size
-    print(clean_feats)
-    print(noise_feats)
+    clean_feats = read_and_decode(TFRecord, 11,
+                                  43)  # 11 means context window size
 
 
 if __name__ == "__main__":
